911659a9e2c2badcb33527a248a1ebab80f8dba2
tracer-ics2023 <tracer@njuics.org>
2023-10-10 16:57:02 +0800

    >  compile NEMU self-study HY Linux debian 6.1.0-13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.55-1 (2023-09-29) x86_64 GNU/Linux  16:57:02 up  3:23,  5 users,  load average: 0.15, 0.20, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,6 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_WATCHPOINT
 	if (difftest_wp() == true) {
 		nemu_state.state = NEMU_STOP;
+		printf("Program stop at %lx\n", _this->pc);
 	}
 #endif
 }
