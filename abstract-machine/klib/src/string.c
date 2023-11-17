09bc19a10e1b77be34f86e9d6130c6ed13f4941d
HY <h_rer@foxmail.com>
2023-11-17 14:00:26 +0800

    Fix the string bug in native , ALL TEST PASSED!

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -138,6 +138,8 @@ int memcmp(const void *s1, const void *s2, size_t n) {
 	size_t i = 0;
 
 	while (i < n && *((unsigned char *)s1 + i) == *((unsigned char *)s2 + i)) ++i;
+	if (i == n)
+		--i;//other wise the i will be in the first diff pos.
 
 	return *((unsigned char *)s1 + i) - *((unsigned char *)s2 + i);
 
