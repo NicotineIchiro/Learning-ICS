10a7a37878422c92329276bb76c25561739a110d
HY <h_rer@foxmail.com>
2023-12-07 16:06:34 +0800

    Fix some UW-ext instruction bug. alu-test passed.

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,10 +4,76 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+size_t i2str(int, char *);
 int printf(const char *fmt, ...)
 {
-	//char *temp;
+		if (fmt == NULL) {
+		panic("NULL fmt!");
+	}
+	// should the format use regex?
+	va_list ap;
+	int temp = 0;
+	char *temps;
+	size_t i = 0;
+	int cnt = 0;
+	int errf = 0;
+
+	va_start(ap, fmt);
+		//size_t flen = strlen(fmt);
+
+	while (fmt[i] != '\0')//i in s0, flen in s3.
+	{
+		if (fmt[i] != '%')
+		{
+			//out[cnt] = fmt[i];
+			putch(fmt[i]);
+			++i;
+			++cnt;//cnt in s1
+		}
+		else
+		{
+			switch (fmt[i + 1])
+			{
+			case 'd':
+				temp = va_arg(ap, int);//the va_arg for %d is int!
+				char i2s[64] = {0};
+				size_t test_len = i2str(temp, i2s);
+				size_t islen = strlen(i2s);
+				if (test_len != islen)
+					panic("i2s bug!");
+				//strncpy(out + cnt, i2s, islen);
+				for (size_t i = 0; i < islen; ++i) {
+					putch(i2s[i]);
+				}
+				i += 2;
+				cnt += islen;
+				break;
+			case 's':
+				temps = va_arg(ap, char *);
+				size_t slen = strlen(temps);
+				//strncpy(out + cnt, temps, slen);
+				for (size_t i = 0; i < slen; ++i) {
+					putch(temps[i]);
+				}
+				i += 2;
+				cnt += slen;
+				break;
+			default:
+				errf = 1;
+				break;
+			}
+		}
+		if (errf == 1)
+		{
+			panic("Invalid format.");
+			return -1;
+		}
+	}
+	
+	//out[cnt] = '\0';
+	va_end(ap);
+
+	return cnt;//char *temp;
 
 	panic("Not implemented");
 }
