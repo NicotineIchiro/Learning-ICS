95cc457d771d6fa5e4e78a01f5f92e0dc3a760d8
HY <h_rer@foxmail.com>
2023-12-18 15:32:42 +0800

    First version of malloc.

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,7 +1,7 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
-
+//#include <nemu.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
@@ -28,15 +28,34 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-
+static int malloc_cnt = 0;
+extern Area heap;
+////extern Area heap = RANGE(&_heap_start, PMEM_END);
+//extern char _heap_start;
+//static char * heap_ptr = &_heap_start;
+static void * hbrk;
+void malloc_reset() {
+	//hbrk = (char *)ROUNDUP(heap.start, 8);
+	hbrk = heap.start;
+}
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+	if (malloc_cnt == 0) {
+		malloc_reset();
+		malloc_cnt++;
+	}
+
+	size = ROUNDUP(size, 8);
+	void * old = hbrk;
+	hbrk += size;
+	assert(heap.start <= hbrk && hbrk < heap.end);
+	return old;
+ //panic("Not implemented");
 #endif
-  return NULL;
+	return NULL;
 }
 
 void free(void *ptr) {
